3122b54293dfb511e6b180e55cef376a1c0ad19d
Yuki Kawaguchi <gucci4237@outlook.jp>
2018-05-10 09:45:24 +0900

    saitekika2

--- share_program/prg/saitekika.py
+++ share_program/prg/saitekika.py
@@ -5,21 +5,35 @@ def fileio(filename):
     assembly=[]
     tmp = []
     i=0
+    t = 0
+    proglines=0
     with open(filename+'.asm','r') as f:
         for line in f.readlines():
-            line=line.strip()
-            line = line.replace(',',' ')
-            spline = line.split()
-            assembly.append(spline)
+            linetmp=line.strip()
+            linetmp = linetmp.replace(',',' ')
+            spline = linetmp.split()
             if(spline[0]=="store" and spline[1] == "R0"):
-                i+=1
                 temp = spline[2]
                 print(spline)
-                #print(spline)
-            if(spline[0]=="load" and i==1 and temp == spline[2]):
-                print(spline)
+                falsestr=line
+                i+=1
+
+                print(i)
+            if(spline[0]=="load" and i==1 and spline[1] == "R0" and temp == spline[2]):
+                #print(proglines)
+                print(assembly[len(assembly)-1])
+                del assembly[len(assembly)-1]
                 i=0
-        #print(assembly)
+            else:
+                assembly.append(line)
+                if(i==1 and spline[0] != "store"):
+                    i=0
+            proglines+=1
+    outfile=open(filename+"kai.asm",'w')
+    for i in assembly:
+        outfile.write(i)
+    outfile.close()
+    #print(assembly)
 def main():
     print("Input:")
     inp = raw_input()
